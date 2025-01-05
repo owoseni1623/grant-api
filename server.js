26fa261c20f07b909b368695548b98221ced102d
god_is_able <owoseni0816@gmail.com>
2025-01-05 09:07:01 -0800

    local

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const app = express();
 const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
-      'http://localhost:5173',
+      // 'http://localhost:5173',
       'https://grant-pi.vercel.app/',
-      'http://localhost:3000',
-      'http://127.0.0.1:3000'
+      // 'http://localhost:3000',
+      // 'http://127.0.0.1:3000'
     ];
 
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
